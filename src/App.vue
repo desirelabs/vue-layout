c49385c271f7472cfc9101433434c529343b80ca
Franck LEBAS <franck.lebas@betao.se>
2023-03-06 08:49:49 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,217 @@
+<template>
+  <div class="container-fluid">
+    <div class="row min-vh-100">
+      <div class="col-12 col-lg-3" ref="left-column">
+        <aside class="sidebar sticky-top">
+          <div class="sidebar__header sticky-top">HEADER</div>
+          <div class="sidebar__body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus
+            urna sed turpis sagittis, et cursus sem lacinia. Mauris interdum dui
+            ac libero blandit aliquet. Donec vehicula vel ante eget ornare. In
+            gravida vel nibh in auctor. Praesent vehicula, tortor et ullamcorper
+            pharetra, libero erat facilisis orci, et tristique mauris felis nec
+            quam. Mauris aliquam metus id lorem semper feugiat. Integer luctus
+            nunc hendrerit tellus pellentesque auctor. Cras vel euismod diam.
+            Fusce rhoncus, lorem ut ultrices rhoncus, metus mi eleifend dui, non
+            lobortis massa augue nec purus. Nam aliquam nibh nec ipsum tempor
+            euismod. Vestibulum condimentum tortor et ligula tristique pulvinar.
+            In hac habitasse platea dictumst. Mauris malesuada molestie lorem.
+            Nunc feugiat sagittis est non pellentesque. Ut quis orci non ipsum
+            varius sollicitudin. Proin venenatis non quam in dapibus. Phasellus
+            non suscipit est. Vivamus lorem lorem, interdum eget elementum non,
+            hendrerit ac nulla. Aliquam maximus ullamcorper augue vitae dignissim.
+            Integer ultricies ipsum vitae justo rhoncus, vestibulum efficitur dui
+            posuere. Duis sapien ipsum, euismod et mauris eu, mattis lacinia
+            massa. Praesent efficitur ipsum et massa cursus, in cursus lectus
+            luctus. Mauris sed mi posuere quam sodales vehicula ac vel ante.
+            Aenean cursus, lectus sit amet finibus mattis, eros elit imperdiet
+            ipsum, a aliquam orci ante id quam. Maecenas sed diam posuere felis
+            efficitur semper. Donec vel metus quis urna congue posuere. Praesent
+            vel tortor eu nunc luctus feugiat. Praesent quis nulla posuere,
+            interdum orci sit amet, congue mi. Maecenas elementum arcu ut mollis
+            accumsan. Cras varius dolor diam. Nam congue ornare vulputate. Aliquam
+            et libero arcu. Maecenas vel nunc finibus, scelerisque dui id,
+            interdum ex. Nam suscipit pulvinar elit. Cras ut lorem dolor.
+            Pellentesque vitae elit metus. Sed tempor ex eu velit maximus feugiat.
+            Quisque eu mattis ipsum. Aliquam pellentesque dignissim dignissim. Sed
+            ac libero mauris. Donec elementum, odio id consectetur pretium, sem
+            erat gravida nulla, eget tincidunt risus enim id felis. Sed accumsan,
+            elit non sodales interdum, purus purus fermentum nisl, a viverra neque
+            metus et justo. Ut pharetra lacus ipsum. Sed vitae velit vitae arcu
+            venenatis aliquam. Nam efficitur aliquet ipsum, in placerat lectus
+            condimentum quis. Vestibulum nec felis vestibulum, iaculis libero sed,
+            auctor orci. Vestibulum ipsum sem, egestas ut ultricies accumsan,
+            maximus nec magna. Aliquam dolor nibh, rutrum id sapien vel, lobortis
+            condimentum mi. Duis nec semper massa. Cras hendrerit mi eros, id
+            dictum mi condimentum at. Pellentesque semper fermentum odio id
+            sollicitudin. Nunc in nunc mi. Vestibulum vel suscipit quam, dictum
+            aliquam justo. Mauris posuere aliquam magna in maximus.
+          </div>
+          <footer class="sidebar__footer sticky-bottom">the footer</footer>
+        </aside>
+      </div>
+      <div class="col-12 col-lg-8" ref="right-column">
+        <main class="main w-100">
+          <nav class="navbar bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Sticky top</a>
+            </div>
+          </nav>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus
+          urna sed turpis sagittis, et cursus sem lacinia. Mauris interdum dui ac
+          libero blandit aliquet. Donec vehicula vel ante eget ornare. In gravida
+          vel nibh in auctor. Praesent vehicula, tortor et ullamcorper pharetra,
+          libero erat facilisis orci, et tristique mauris felis nec quam. Mauris
+          aliquam metus id lorem semper feugiat. Integer luctus nunc hendrerit
+          tellus pellentesque auctor. Cras vel euismod diam. Fusce rhoncus, lorem ut
+          ultrices rhoncus, metus mi eleifend dui, non lobortis massa augue nec
+          purus. Nam aliquam nibh nec ipsum tempor euismod. Vestibulum condimentum
+          tortor et ligula tristique pulvinar. In hac habitasse platea dictumst.
+          Mauris malesuada molestie lorem. Nunc feugiat sagittis est non
+          pellentesque. Ut quis orci non ipsum varius sollicitudin. Proin venenatis
+          non quam in dapibus. Phasellus non suscipit est. Vivamus lorem lorem,
+          interdum eget elementum non, hendrerit ac nulla. Aliquam maximus
+          ullamcorper augue vitae dignissim. Integer ultricies ipsum vitae justo
+          rhoncus, vestibulum efficitur dui posuere. Duis sapien ipsum, euismod et
+          mauris eu, mattis lacinia massa. Praesent efficitur ipsum et massa cursus,
+          in cursus lectus luctus. Mauris sed mi posuere quam sodales vehicula ac
+          vel ante. Aenean cursus, lectus sit amet finibus mattis, eros elit
+          imperdiet ipsum, a aliquam orci ante id quam. Maecenas sed diam posuere
+          felis efficitur semper. Donec vel metus quis urna congue posuere. Praesent
+          vel tortor eu nunc luctus feugiat. Praesent quis nulla posuere, interdum
+          orci sit amet, congue mi. Maecenas elementum arcu ut mollis accumsan. Cras
+          varius dolor diam. Nam congue ornare vulputate. Aliquam et libero arcu.
+          Maecenas vel nunc finibus, scelerisque dui id, interdum ex. Nam suscipit
+          pulvinar elit. Cras ut lorem dolor. Pellentesque vitae elit metus. Sed
+          tempor ex eu velit maximus feugiat. Quisque eu mattis ipsum. Aliquam
+          pellentesque dignissim dignissim. Sed ac libero mauris. Donec elementum,
+          odio id consectetur pretium, sem erat gravida nulla, eget tincidunt risus
+          enim id felis. Sed accumsan, elit non sodales interdum, purus purus
+          fermentum nisl, a viverra neque metus et justo. Ut pharetra lacus ipsum.
+          Sed vitae velit vitae arcu venenatis aliquam. Nam efficitur aliquet ipsum,
+          in placerat lectus condimentum quis. Vestibulum nec felis vestibulum,
+          iaculis libero sed, auctor orci. Vestibulum ipsum sem, egestas ut
+          ultricies accumsan, maximus nec magna. Aliquam dolor nibh, rutrum id
+          sapien vel, lobortis condimentum mi. Duis nec semper massa. Cras hendrerit
+          mi eros, id dictum mi condimentum at. Pellentesque semper fermentum odio
+          id sollicitudin. Nunc in nunc mi. Vestibulum vel suscipit quam, dictum
+          aliquam justo. Mauris posuere aliquam magna in maximus. Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Fusce dapibus urna sed turpis
+          sagittis, et cursus sem lacinia. Mauris interdum dui ac libero blandit
+          aliquet. Donec vehicula vel ante eget ornare. In gravida vel nibh in
+          auctor. Praesent vehicula, tortor et ullamcorper pharetra, libero erat
+          facilisis orci, et tristique mauris felis nec quam. Mauris aliquam metus
+          id lorem semper feugiat. Integer luctus nunc hendrerit tellus pellentesque
+          auctor. Cras vel euismod diam. Fusce rhoncus, lorem ut ultrices rhoncus,
+          metus mi eleifend dui, non lobortis massa augue nec purus. Nam aliquam
+          nibh nec ipsum tempor euismod. Vestibulum condimentum tortor et ligula
+          tristique pulvinar. In hac habitasse platea dictumst. Mauris malesuada
+          molestie lorem. Nunc feugiat sagittis est non pellentesque. Ut quis orci
+          non ipsum varius sollicitudin. Proin venenatis non quam in dapibus.
+          Phasellus non suscipit est. Vivamus lorem lorem, interdum eget elementum
+          non, hendrerit ac nulla. Aliquam maximus ullamcorper augue vitae
+          dignissim. Integer ultricies ipsum vitae justo rhoncus, vestibulum
+          efficitur dui posuere. Duis sapien ipsum, euismod et mauris eu, mattis
+          lacinia massa. Praesent efficitur ipsum et massa cursus, in cursus lectus
+          luctus. Mauris sed mi posuere quam sodales vehicula ac vel ante. Aenean
+          cursus, lectus sit amet finibus mattis, eros elit imperdiet ipsum, a
+          aliquam orci ante id quam. Maecenas sed diam posuere felis efficitur
+          semper. Donec vel metus quis urna congue posuere. Praesent vel tortor eu
+          nunc luctus feugiat. Praesent quis nulla posuere, interdum orci sit amet,
+          congue mi. Maecenas elementum arcu ut mollis accumsan. Cras varius dolor
+          diam. Nam congue ornare vulputate. Aliquam et libero arcu. Maecenas vel
+          nunc finibus, scelerisque dui id, interdum ex. Nam suscipit pulvinar elit.
+          Cras ut lorem dolor. Pellentesque vitae elit metus. Sed tempor ex eu velit
+          maximus feugiat. Quisque eu mattis ipsum. Aliquam pellentesque dignissim
+          dignissim. Sed ac libero mauris. Donec elementum, odio id consectetur
+          pretium, sem erat gravida nulla, eget tincidunt risus enim id felis. Sed
+          accumsan, elit non sodales interdum, purus purus fermentum nisl, a viverra
+          neque metus et justo. Ut pharetra lacus ipsum. Sed vitae velit vitae arcu
+          venenatis aliquam. Nam efficitur aliquet ipsum, in placerat lectus
+          condimentum quis. Vestibulum nec felis vestibulum, iaculis libero sed,
+          auctor orci. Vestibulum ipsum sem, egestas ut ultricies accumsan, maximus
+          nec magna. Aliquam dolor nibh, rutrum id sapien vel, lobortis condimentum
+          mi. Duis nec semper massa. Cras hendrerit mi eros, id dictum mi
+          condimentum at. Pellentesque semper fermentum odio id sollicitudin. Nunc
+          in nunc mi. Vestibulum vel suscipit quam, dictum aliquam justo. Mauris
+          posuere aliquam magna in maximus. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Fusce dapibus urna sed turpis sagittis, et cursus sem
+          lacinia. Mauris interdum dui ac libero blandit aliquet. Donec vehicula vel
+          ante eget ornare. In gravida vel nibh in auctor. Praesent vehicula, tortor
+          et ullamcorper pharetra, libero erat facilisis orci, et tristique mauris
+          felis nec quam. Mauris aliquam metus id lorem semper feugiat. Integer
+          luctus nunc hendrerit tellus pellentesque auctor. Cras vel euismod diam.
+          Fusce rhoncus, lorem ut ultrices rhoncus, metus mi eleifend dui, non
+          lobortis massa augue nec purus. Nam aliquam nibh nec ipsum tempor euismod.
+          Vestibulum condimentum tortor et ligula tristique pulvinar. In hac
+          habitasse platea dictumst. Mauris malesuada molestie lorem. Nunc feugiat
+          sagittis est non pellentesque. Ut quis orci non ipsum varius sollicitudin.
+          Proin venenatis non quam in dapibus. Phasellus non suscipit est. Vivamus
+          lorem lorem, interdum eget elementum non, hendrerit ac nulla. Aliquam
+          maximus ullamcorper augue vitae dignissim. Integer ultricies ipsum vitae
+          justo rhoncus, vestibulum efficitur dui posuere. Duis sapien ipsum,
+          euismod et mauris eu, mattis lacinia massa. Praesent efficitur ipsum et
+          massa cursus, in cursus lectus luctus. Mauris sed mi posuere quam sodales
+          vehicula ac vel ante. Aenean cursus, lectus sit amet finibus mattis, eros
+          elit imperdiet ipsum, a aliquam orci ante id quam. Maecenas sed diam
+          posuere felis efficitur semper. Donec vel metus quis urna congue posuere.
+          Praesent vel tortor eu nunc luctus feugiat. Praesent quis nulla posuere,
+          interdum orci sit amet, congue mi. Maecenas elementum arcu ut mollis
+          accumsan. Cras varius dolor diam. Nam congue ornare vulputate. Aliquam et
+          libero arcu. Maecenas vel nunc finibus, scelerisque dui id, interdum ex.
+          Nam suscipit pulvinar elit. Cras ut lorem dolor. Pellentesque vitae elit
+          metus. Sed tempor ex eu velit maximus feugiat. Quisque eu mattis ipsum.
+          Aliquam pellentesque dignissim dignissim. Sed ac libero mauris. Donec
+          elementum, odio id consectetur pretium, sem erat gravida nulla, eget
+          tincidunt risus enim id felis. Sed accumsan, elit non sodales interdum,
+          purus purus fermentum nisl, a viverra neque metus et justo. Ut pharetra
+          lacus ipsum. Sed vitae velit vitae arcu venenatis aliquam. Nam efficitur
+          aliquet ipsum, in placerat lectus condimentum quis. Vestibulum nec felis
+          vestibulum, iaculis libero sed, auctor orci. Vestibulum ipsum sem, egestas
+          ut ultricies accumsan, maximus nec magna. Aliquam dolor nibh, rutrum id
+          sapien vel, lobortis condimentum mi. Duis nec semper massa. Cras hendrerit
+          mi eros, id dictum mi condimentum at. Pellentesque semper fermentum odio
+          id sollicitudin. Nunc in nunc mi. Vestibulum vel suscipit quam, dictum
+          aliquam justo. Mauris posuere aliquam magna in maximus. lorem ipsum
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  mounted() {
+    const leftColumn = document.querySelector('.col-lg-4');
+    const rightColumn = document.querySelector('.col-lg-8');
+
+    this.$refs['left-column'].addEventListener('scroll', function () {
+      rightColumn.scrollTop = leftColumn.scrollTop;
+    });
+
+    this.$refs['right-column'].addEventListener('scroll', function () {
+      leftColumn.scrollTop = rightColumn.scrollTop;
+    });
+  }
+}
+</script>
+<style scoped>
+.sidebar {
+  max-width: 260px;
+  height: 100%;
+}
+[class*="col-"] {
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch
+}
+.navbar {
+  position: sticky;
+  top: 0;
+}
+@media only screen and (min-width: 1200px) {
+  .navbar {
+    position: relative;
+  }
+}
+</style>
